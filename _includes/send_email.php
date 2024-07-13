d5f9dafd05029a5c8ff112378d95e3270a3a801b
Azizah Giwa <azizahgiwa@gmail.com>
2024-07-13 18:49:32 +0100

    Create send_email.php

---
+++ _includes/send_email.php
@@ -0,0 +1,29 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    echo "No arguments Provided!";
+    return false;
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+// Create the email and send the message
+$to = 'azizahgiwa@gmail.com'; // Replace with your email address
+$email_subject = "Website Contact Form: $name";
+$email_body = "You have received a new message from your website contact form.\n\n".
+              "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // Replace with your domain's noreply email
+$headers .= "Reply-To: $email_address";
+
+// Send the email
+if(mail($to, $email_subject, $email_body, $headers)) {
+    echo "Mail Sent!";
+    return true;
+} else {
+    echo "Mail Not Sent!";
+    return false;
+}
+?>
